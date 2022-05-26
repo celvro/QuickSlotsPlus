29b6c68f610a3af6ec79d29852945718e0d0f62f
Dillon Lustick <lustickd@gmail.com>
2022-05-26 15:52:12 -0600

    Simplify the OnAddItem patch

--- Patches/QuickSlots_OnAddItem_Patch.cs
+++ Patches/QuickSlots_OnAddItem_Patch.cs
@@ -12,25 +12,7 @@ namespace QuickSlotsPlus.Patches
     {
         static bool Prefix(QuickSlots __instance, InventoryItem item)
         {
-            Logger.Log(Logger.Level.Debug, $"Tech type is {item.item.GetTechType()}");
-            if (item.item.GetTechType() == TechType.FireExtinguisher)
-            {
-                Logger.Log(Logger.Level.Debug, $"Picked up fire extinguisher!");
-                // Add exception if bindings are empty so you can use the FireExtinguisher at the beginning of the game
-                // It won't let you use the PDA until after you put out the fire
-                for (int i = 0; i < __instance.binding.Length; i++)
-                {
-                    if (__instance.binding[i] != null)
-                    {
-                        Logger.Log(Logger.Level.Debug, "Bindings are not empty!");
-                        return !Mod.Config.disableBindToEmpty;
-                    }
-                }
-                Logger.Log(Logger.Level.Debug, "Binding fire extinguisher so you can get through the intro.");
-                return true;
-            }
-
-            return !Mod.Config.disableBindToEmpty;
+            return !Mod.Config.disableBindToEmpty || IntroVignette.isIntroActive;
         }
     }
 }

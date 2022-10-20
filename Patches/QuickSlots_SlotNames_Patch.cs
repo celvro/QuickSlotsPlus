4306edad792d03fa3104622d7598bd37e52605d2
celvro <lustickd@gmail.com>
2022-10-19 22:38:07 -0500

    Fix bug deselecting held item
    Subnautica 1 uses different opcodes in the DeselectImmediate function

--- Patches/QuickSlots_SlotNames_Patch.cs
+++ Patches/QuickSlots_SlotNames_Patch.cs
@@ -66,7 +66,7 @@ namespace QuickSlotsPlus.Patches
             var codes = new List<CodeInstruction>(instructions);
             for (var i = 0; i < codes.Count; i++)
             {
-                if (codes[i].opcode == OpCodes.Ldsfld && codes[i + 1].opcode == OpCodes.Ldloc_0)
+                if (codes[i].opcode == OpCodes.Ldsfld && codes[i + 1].opcode == OpCodes.Ldloc_1)
                 {
                     codes[i].operand = AccessTools.Field(typeof(LabelUtil), "slotNames");
                     Logger.Log(Logger.Level.Info, "Transpiled DeselectInternal method with new slotnames.");
